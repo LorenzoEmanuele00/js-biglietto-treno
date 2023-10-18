1590ef93597c06ef302f8f24d633dd61b586b038
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-10-18 15:52:02 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// DATA
+const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+console.log(km);
+
+const eta = parseInt(prompt("Quanti anni hai?"));
+console.log(eta);
+
+const kmPrice = 0.21;
+console.log(kmPrice);
+
+const minorDiscount = 0.2;
+console.log(minorDiscount);
+
+const overDiscount = 0.4
+console.log(overDiscount);
+
+
+// LOGIC
+let ticket;
+ticket = km * kmPrice;
+console.log(ticket);
+
+if (eta < 18) {
+    ticket = ticket * minorDiscount;
+} else if (eta >= 65) {
+    ticket = ticket * overDiscount;
+}
+console.log(ticket);
+
+ticket = ticket.toFixed(2)  // reduce to 2 decimal
+console.log(ticket);
+
+const message = 
+    `<h2>Ciao</h2>
+    <p>Questo è il costo del tuo biglietto: ${ticket}</p>
+    <p>Buon viaggio!</p>`
+;
+
+
+// OUTPUT
+document.getElementById("train-ticket").innerHTML = message;
--- readme.md
+++ readme.md
@@ -11,6 +11,7 @@
 1. Calcolare il prezzo base del biglietto
 2. Calcolare il prezzo del biglietto con lo sconto SE l'età è adeguata
 3. Modificare il prezzo affinche ci siano solo due decimali nel numero 
+4. Preparare il messaggio con il prezzo del biglietto
 
 
 **OUTPUT :**
